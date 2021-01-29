e3ea1c5b27571ce4e380f9a688dbb6696d3551cb
vassilismarougkas <62342499+vassilismarougkas@users.noreply.github.com>
2021-01-29 13:49:22 +0200

    Update info.lua

--- programs/info.lua
+++ programs/info.lua
@@ -8,9 +8,16 @@ local drive = require("component").proxy(computer.getBootAddress())
 
 print("System Info")
 print("Version: "..version.getVersion())
-print("Storage: "..drive.spaceUsed().."/"..drive.spaceTotal().."  ("..(math.floor(1000*drive.spaceUsed()/drive.spaceTotal())/10).."%)")
 
-local usedMemory = computer.totalMemory() - computer.freeMemory()
-print("Memory: "..usedMemory.."/"..computer.totalMemory().."  ("..(math.floor(1000*usedMemory/computer.totalMemory())/10).."%)")
+local drive_used = drive.spaceUsed()
+local drive_total = drive.spaceTotal()
+local drive_used_percentage = 100*drive_used/drive_total
+print(string.format( "Storage: %d/%d Used (%.2f)", drive_used, drive_total, drive_used_percentage))
+
+local ram_free = computer.freeMemory()
+local ram_total = computer.totalMemory()
+local ram_used = ram_total - ram_free
+local ram_used_percentage = 100*ram_used/ram_total
+print(string.format( "Memory: %d/%d Used (%.2f)", ram_used, ram_total, ram_used_percentage))
 
 shell.setWorkingDirectory(currentDirectory)
