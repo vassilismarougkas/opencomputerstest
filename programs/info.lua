0b73b47b18db3d959f2c595454e301620acebbf2
vassilismarougkas <62342499+vassilismarougkas@users.noreply.github.com>
2021-01-29 13:51:27 +0200

    Update info.lua

--- programs/info.lua
+++ programs/info.lua
@@ -12,12 +12,12 @@ print("Version: "..version.getVersion())
 local drive_used = drive.spaceUsed()
 local drive_total = drive.spaceTotal()
 local drive_used_percentage = 100*drive_used/drive_total
-print(string.format( "Storage: %d/%d Used (%.2f)", drive_used, drive_total, drive_used_percentage))
+print(string.format( "Storage: %d/%d (%.2f%%)", drive_used, drive_total, drive_used_percentage))
 
 local ram_free = computer.freeMemory()
 local ram_total = computer.totalMemory()
 local ram_used = ram_total - ram_free
 local ram_used_percentage = 100*ram_used/ram_total
-print(string.format( "Memory: %d/%d Used (%.2f)", ram_used, ram_total, ram_used_percentage))
+print(string.format( "Memory: %d/%d (%.2f%%)", ram_used, ram_total, ram_used_percentage))
 
 shell.setWorkingDirectory(currentDirectory)
