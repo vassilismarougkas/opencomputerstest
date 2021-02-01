fe9d5ca30e513d6a51a67fda4d0617e3ed63e86f
vassilismarougkas <62342499+vassilismarougkas@users.noreply.github.com>
2021-02-01 07:32:11 +0200

    Update flashloader.lua

--- apt/programs/flashloader/flashloader.lua
+++ apt/programs/flashloader/flashloader.lua
@@ -15,7 +15,7 @@ end
 local function printUsage()
     local st = [[
     Flashloader Usage:
-        flashloader flash <name>
+        flashloader flash <name> [label]
         flashloader update
         flashloader download <name>
     ]]
@@ -27,11 +27,11 @@ local function update()
     repeat
         local file = list()
         if not file == nil then
-            download("/data/flash/"..file, "/apt/flashloader/flashes/"..file)
+            download("/data/flash/"..file, "/apt/programs/flashloader/flashes/"..file)
         end
     until file == nil
     list = nil
-    
+
 end
 
 if (#tArgs == 0) then
@@ -49,17 +49,27 @@ end
 
 if (#tArgs == 2) then
     if (tArgs[1] == "download") then
-        download("/data/flash/"..tArgs[2], "/apt/flashloader/flashes/"..tArgs[2])
+        download("/data/flash/"..tArgs[2], "/apt/programs/flashloader/flashes/"..tArgs[2])
         return nil
     end
     if (tArgs[1] == "flash") then
         if not filesystem.exists("flash -q /data/flash/"..tArgs[2]) then
-            download("/data/flash/"..tArgs[2], "/apt/flashloader/flashes/"..tArgs[2])
+            download("/data/flash/"..tArgs[2], "/apt/programs/flashloader/flashes/"..tArgs[2])
         end
-        shell.execute("flash -q /data/flash/"..tArgs[2])
+        shell.execute("flash -q /data/flash/"..tArgs[2].." "..tArgs[2])
         return nil
     end
     printUsage()
 end
 
+if (#tArgs == 3) then
+    if (tArgs[1] == "flash") then
+        if not filesystem.exists("flash -q /data/flash/"..tArgs[2]) then
+            download("/data/flash/"..tArgs[2], "/apt/programs/flashloader/flashes/")
+        end
+        shell.execute("flash -q /data/flash/"..tArgs[2].." "..tArgs[3])
+        return nil
+    end
+    printUsage()
+end
 printUsage()
