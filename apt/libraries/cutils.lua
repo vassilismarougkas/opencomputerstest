ca51c132645f3ec8f4ecfc2032925b159c2bd05d
vassilismarougkas <62342499+vassilismarougkas@users.noreply.github.com>
2021-02-03 01:49:41 +0200

    cutils update

--- apt/libraries/cutils.lua
+++ apt/libraries/cutils.lua
@@ -2,14 +2,11 @@ local cutils = {}
 
 local function findComponent(Address)
     local component = require("component")
-    local iter = component.list()
-    local comp = nil
-    repeat
-        comp = iter()
-        if comp:sub(1, #Address) == Address then
-            return component.proxy(comp)
+    for addr in pairs(component.list()) do
+        if addr:sub(1, #Address) == Address then
+            return component.proxy(addr)
         end
-    until comp == nil
+    end
     return nil
 end
 
